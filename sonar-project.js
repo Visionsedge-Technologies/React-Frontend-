49e39176db8f91f90d5149af69bdd08f643226a8
Deji Hamed <111803982+dhamed007@users.noreply.github.com>
2022-11-22 23:27:24 +0800

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://13.59.196.208:7000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': 'squ_7d24dd1e8ab896e1048111eefabd496b15c667e0',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'temitope101!',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
